dec2e8dd3a4b05be4b797bc0918f73b3d850cd13
dogpf <92705030+dogpf@users.noreply.github.com>
2021-10-18 20:25:19 +0800

    Add files via upload

---
+++ L8/yyy.java
@@ -0,0 +1,17 @@
+class Caaa 
+{
+	int a;
+	int b;
+	int c;
+}
+public class yyy {
+
+	public static void main(String[] args) {
+		Caaa obj=new Caaa();
+		obj.a=5;
+		obj.b=3;
+		obj.c=obj.a*obj.b;
+	System.out.println("a="+obj.a+" b="+obj.b+" c="+obj.c);			
+	}
+
+}
