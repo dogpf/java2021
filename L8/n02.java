33b6528797b59a83676611fb7580a494077ac17d
dogpf <92705030+dogpf@users.noreply.github.com>
2021-10-29 17:55:35 +0800

    Add files via upload

---
+++ L8/n02.java
@@ -0,0 +1,26 @@
+class Cbbb
+{
+	double x;
+	double y;
+}
+public class n02 {
+	public static void main(String[] args) {
+ Cbbb obj1=new Cbbb();
+ Cbbb obj2=new Cbbb();
+ Cbbb avg=new Cbbb();
+    obj1.x=5.2;
+    obj1.y=3.9;
+    obj2.x=6.5;
+    obj2.y=4.6;
+     
+    avg.x=(obj1.x+obj2.x)/2;
+    avg.y=(obj1.y+obj2.y)/2;
+   System.out.println("obj1.x="+obj1.x+"obj1.y="+obj1.y+"obj2.x="+obj2.x+"obj2.y="+obj2.y+"avg.x="+avg.x+"avg.y="+avg.y);
+   
+   
+     
+    
+    		
+
+	}
+}
