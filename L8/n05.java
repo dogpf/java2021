47a3eace487b4c14dec0d12546051cf04da5edfc
dogpf <92705030+dogpf@users.noreply.github.com>
2021-10-31 10:16:36 +0800

    Add files via upload

---
+++ L8/n05.java
@@ -0,0 +1,38 @@
+package fff;
+
+public class n05 {
+	 class CBox 
+	 { 
+	 int length; 
+	 int width; 
+	 int height; 
+	 int volume() 
+	 { 
+	 return length*width*height; 
+	 } 
+	 int surfaceArea() 
+	 { 
+	 return 2*((length*width)+(width*height)+(length*height)); 
+	 } 
+	 void showData() 
+	 { 
+	 System.out.println("length="+length); 
+	 System.out.println("width="+width); 
+	 System.out.println("height="+height); 
+	 } 
+	 void showAll() 
+	 { 
+	 showData(); 
+	 System.out.println("surface area="+surfaceArea()); 
+	 System.out.println("volume="+volume()); 
+	 }  
+	 } 
+	public class class05
+	 { 
+	 public static void main(String args[]) 
+	 { 
+	 CBox box; box=new CBox(); 
+	}
+	}
+
+}
