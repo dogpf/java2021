af1eac95e158d3d6fbb6043b402dd28af6d9d939
dogpf <92705030+dogpf@users.noreply.github.com>
2021-10-31 10:15:28 +0800

    Add files via upload

---
+++ n06.java
@@ -0,0 +1,21 @@
+package ggg;
+
+public class n06 {class CCircle 
+	{ 
+	 double pi=3.14; 
+	 double radius; 
+	 void show_periphery() 
+	{System.out.println("periphery="+2*this.pi*this.radius);
+	} 
+	} 
+	public class class06
+	{ 
+	public static void main(String args[]) 
+	{ 
+	CCircle cir1=new CCircle(); 
+	cir1.radius=3.0; 
+	cir1.show_periphery();  
+	} 
+	}
+
+}
